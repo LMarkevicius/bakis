665dc821a62e43f36c5b5ab4bbb4823308ae0651
Lukas Markevicius <vysniukass@gmail.com>
2018-05-07 20:12:11 +0000

    cron hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('CheckForNewLunches')->runInBackground()->everyMinute();
+        $schedule->command('CheckForNewLunches')->runInBackground()->hourly();
     }
 
     /**
